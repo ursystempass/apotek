832d9a51894386de0da76c4055c2e530a2fcbd65
ursystempass <jungw0npepsi@gmail.com>
2024-03-18 10:41:31 +0700

    feat: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,54 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
+        /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       DB::table('users')->insert([
+            'name' => 'apoteker',
+            'email' => 'apoteker@gmail.com',
+            'password' => Hash::make('2Juli2004!'),
+            'level' => 'apoteker',
+            'aktif' => '1',
+            'created_at' => now(),
+            'updated_at' => now(),
+       ]);
+       DB::table('users')->insert([
+            'name' => 'gudang',
+            'email' => 'gudang@gmail.com',
+            'password' => Hash::make('2Juli2004!'),
+            'level' => 'gudang',
+            'aktif' => '1',
+            'created_at' => now(),
+            'updated_at' => now(),
+       ]);
+       DB::table('users')->insert([
+            'name' => 'kasir',
+            'email' => 'kasir@gmail.com',
+            'password' => Hash::make('2Juli2004!'),
+            'level' => 'kasir',
+            'aktif' => '1',
+            'created_at' => now(),
+            'updated_at' => now(),
+       ]);
+       DB::table('users')->insert([
+            'name' => 'pemilik',
+            'email' => 'pemilik@gmail.com',
+            'password' => Hash::make('2Juli2004!'),
+            'level' => 'pemilik',
+            'aktif' => '1',
+            'created_at' => now(),
+            'updated_at' => now(),
+       ]);
     }
 }
